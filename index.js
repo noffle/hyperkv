4000384d8c869f56625a69c59b63f11448bc07a9
James Halliday <mail@substack.net>
2015-12-02 20:11:39 -0600

    query the log instead of saving duplicates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var sub = require('subleveldown')
 var indexer = require('hyperlog-index')
+var once = require('once')
 
 module.exports = KV
 
@@ -10,11 +11,12 @@ function KV (opts) {
   self.idb = sub(opts.db, 'i')
   self.xdb = sub(opts.db, 'x', { valueEncoding: 'json' })
   self.dex = indexer(self.log, self.idb, function (row, next) {
-    self.xdb.get(row.value.k, function (err, doc) {
-      if (!doc) doc = {}
+    self.xdb.get(row.value.k, function (err, keys) {
+      var doc = {}
+      ;(keys || []).forEach(function (key) { doc[key] = true })
       row.links.forEach(function (link) { delete doc[link] })
-      doc[row.key] = row.value.v
-      self.xdb.put(row.value.k, doc, next)
+      doc[row.key] = true
+      self.xdb.put(row.value.k, Object.keys(doc), next)
     })
   })
 }
@@ -32,17 +34,31 @@ KV.prototype.put = function (key, value, opts, cb) {
   if (opts.links) {
     self.log.add(opts.links, doc, cb)
   } else {
-    self.get(key, function (err, values) {
-      if (err && !notFound(err)) cb(err)
-      else self.log.add(Object.keys(values || {}), doc, cb)
+    self.dex.ready(function () {
+      self.xdb.get(key, function (err, links) {
+        if (err && !notFound(err)) cb(err)
+        else self.log.add(links || [], doc, cb)
+      })
     })
   }
 }
 
 KV.prototype.get = function (key, cb) {
   var self = this
+  cb = once(cb || noop)
   self.dex.ready(function () {
-    self.xdb.get(key, cb)
+    self.xdb.get(key, function (err, links) {
+      var values = {}
+      if (!links) links = []
+      var pending = links.length
+      links.forEach(function (link) {
+        self.log.get(link, function (err, doc) {
+          if (err) return cb(err)
+          values[link] = doc.value.v
+          if (--pending === 0) cb(null, values)
+        })
+      })
+    })
   })
 }
 
